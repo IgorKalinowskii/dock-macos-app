52a86dec4a7a76fe1c0dd9192c8d19ee8392d607
IgorKalinowskii <s15046@pjwstk.edu.pl>
2025-01-10 09:14:41 +0100

    Fix base href for GitHub Pages

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4135116581"
+    serviceWorkerVersion: "2614052240"
   }
 });
